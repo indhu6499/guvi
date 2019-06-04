f91a66644606aea920fbb4c0f44fb909a0e8579a
indhu6499 <51354194+indhu6499@users.noreply.github.com>
2019-06-04 15:17:46 +0530

    Create lar.py

---
+++ code/lar.py
@@ -0,0 +1,9 @@
+var=input("enter the value")
+var1=input("enter the value")
+var2=input("enter the value")
+if(var>var1)and(var>var2):
+    print(var)
+elif(var1>var)and(var1>var2):
+    print(var1)
+else:
+    print(var2)
