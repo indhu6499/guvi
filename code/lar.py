62b730b1a638704a25162f3db4e7d1864929b1bf
indhu6499 <51354194+indhu6499@users.noreply.github.com>
2019-06-04 15:18:49 +0530

    Update lar.py

--- code/lar.py
+++ code/lar.py
@@ -1,6 +1,6 @@
-var=input("enter the value")
-var1=input("enter the value")
-var2=input("enter the value")
+var=input()
+var1=input()
+var2=input()
 if(var>var1)and(var>var2):
     print(var)
 elif(var1>var)and(var1>var2):
