7bb79797cc80af8b0dd52477b91ef1193f36f724
indhu6499 <51354194+indhu6499@users.noreply.github.com>
2019-07-25 16:49:14 +0530

    Create set213.py

---
+++ code/set213.py
@@ -0,0 +1,10 @@
+num = (input(" "))
+if num > 1:
+    for i in range(2, num):
+        if (num% i) == 0:
+            print(num, "no")
+            break
+    else:
+        print(num,"yes")
+else:
+    print(num, "no")
