ae3bb4ef682c32c976430c8c30223b59faa711e4
indhu6499 <51354194+indhu6499@users.noreply.github.com>
2019-07-26 16:54:05 +0530

    Create pro14.py

---
+++ code/pro14.py
@@ -0,0 +1,13 @@
+s,i=map(int,input(" ").split(" "))
+lis1=list(map(int,input().split()))
+s=[]
+lis1.insert(0,0)
+for y in range(i):
+     vin=[]
+     a=0
+     bb,zz=map(int,input().split())
+     for i in range(bb,zz+1):         
+         a^=lis1[i]     
+     s.append(s)
+for y in s:
+     print(y)
